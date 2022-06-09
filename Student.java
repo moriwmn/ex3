ed0a2c600a8eb4b4f2d3512497a522f9f7713f41
Shirakaufman <105601178+Shirakaufman@users.noreply.github.com>
2022-06-10 00:13:40 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,7 +3,7 @@ package ex_3;
 public class Student extends Employee{
 	String university;
 	int years; // how many years to graduation
-	
+	int gpa // avarge grades
 	
 	@Override
 	public void changeDetiles() {
