60bed722a3ac234299107d5e3143f1b54c08bbe3
Shirakaufman <105601178+Shirakaufman@users.noreply.github.com>
2022-06-24 00:16:57 +0300

    Update Student_Job.java

--- Student_Job.java
+++ Student_Job.java
@@ -1,16 +1,16 @@
 package ex3;
 
 public class Student_Job extends Job {
-	int salery_per_hour;
-	int gpa_req;
-	int num_of_hours;
-	
+	int _salery_per_hour;
+	int _gpa_req;
+	int _num_of_hours;
+
 	public Student_Job(String type, String company, String location, Languages prog_language, int salery_per_hour,
 			int gpa_req, int num_of_hours) {
 		super(type, company, location, prog_language);
-		this.salery_per_hour = salery_per_hour;
-		this.gpa_req = gpa_req;
-		this.num_of_hours = num_of_hours;
+		this._salery_per_hour = salery_per_hour;
+		this._gpa_req = gpa_req;
+		this._num_of_hours = num_of_hours;
 	}
 
 }
