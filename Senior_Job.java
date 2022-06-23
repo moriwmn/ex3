7c01503c07e0903b1d7be6868e1325cdfe3444b5
Shirakaufman <105601178+Shirakaufman@users.noreply.github.com>
2022-06-24 00:16:33 +0300

    shira

--- Senior_Job.java
+++ Senior_Job.java
@@ -1,10 +1,15 @@
 package ex3;
 
 public class Senior_Job extends Job {
-	 int Seniority;
-	 String experience;
+	int _seniority;
+	String _experience;
 
-	
-	
+	public Senior_Job(String type, String company, String location, Languages prog_language, int seniority,
+			String experience) {
+		super(type, company, location, prog_language);
+		_experience = experience;
+		_seniority = seniority;
+
+	}
 
 }
