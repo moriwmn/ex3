3ff800601c326ce4cc5a6792dbeb72dd173dabf1
Shirakaufman <105601178+Shirakaufman@users.noreply.github.com>
2022-06-24 00:11:18 +0300

    shira
    create func- create_user_card

--- E_Profile.java
+++ E_Profile.java
@@ -1,26 +1,33 @@
 package ex3;
 
+import java.util.Scanner;
+
 public class E_Profile implements Profile {
 
-	private String _user_name;
-	private Employee _employee; //student/ junior
-	
-	public E_Profile(String user_name) {//TODO:figure out why do we need detailes
-		_user_name=_user_name;
-		Personal_info personal_info=new Personal_info(user_name);
-		_employee=new Employee(personal_info);//TODO:why??????????????????????????
-	}
-	public E_Profile() {
-	}
-	
-	public Employee getEmployee(){
-		return _employee;
+	private Employee _employee; // student/ junior
+	Scanner input = new Scanner(System.in); // Create a Scanner object
 
+	public E_Profile() {
+		create_user_card(); // TODO - it is ok?
 	}
-	//getters and setters:
+
+	// public E_Profile(String user_name) {// TODO:figure out why do we need
+	// detailes
+	// _user_name = user_name;
+	// Personal_info personal_info = new Personal_info(user_name);
+	// _employee = new Employee(personal_info);// TODO:why??????????????????????????
+	// }
+
+	// getters and setters:
 	public String getEmployeeName() {
 		return _employee.getDetails().getName();
 	}
+
+	public Employee getEmployee() {
+		return _employee;
+
+	}
+
 	public void setEmployeeName(String name) {
 		_employee.getDetails().setName(name);
 	}
@@ -32,53 +39,76 @@ public class E_Profile implements Profile {
 	public void setEmployeeLocation(String location) {
 		_employee.getDetails().setLocation(location);
 	}
+
 	public String getEmployeeEmail() {
 		return _employee.getDetails().getEmail();
 	}
 
-	public void setEmployeeEmail(String email ) {
+	public void setEmployeeEmail(String email) {
 		_employee.getDetails().setEmail(email);
 	}
+
 	public String getEmployeePhone() {
 		return _employee.getDetails().getPhone();
 	}
 
-	public void setEmployeePhone(String phone ) {
+	public void setEmployeePhone(String phone) {
 		_employee.getDetails().setPhone(phone);
 	}
 
-
-
-
-
-	
-	public void create_account() {
-		
-	
-		}
-
-
-	public String getUser_name() {
-		return _user_name;
-	}
-
-
-	public void setUser_name(String user_name) {
-		this._user_name = user_name;
-	}
-	
-	//func:
-
-
-
+	// func:
 	@Override
 	public void menu() {
-		
 
 	}
-	
 
-	
-	}//end of class
-	
+	public void create_user_card() {
+		System.out.println("create your user_card:");
+		System.out.println("pleas enter your name");
+		String name = input.nextLine();
+		System.out.println("please enter your email");
+		String email = input.nextLine();
+		System.out.println("please enter your location: north,south,center"); // TODO לחשוב איך שומרים את זה בשביל המיון
+																				// אחכ הגהה
+		String location = input.nextLine();
+		System.out.println("please enter your phone number");
+		String phone = input.nextLine();
+		Personal_info personal_inf = new Personal_info(name, email, location, phone);
+		boolean py = false, java = false, c = false, cpp = false, j_s = false;
+		int app = 0;
+		int app2 = 0;
+		do {
+			System.out.println("what programming languages is required?");
+			System.out.println("1)python");
+			System.out.println("2)java");
+			System.out.println("3)c");
+			System.out.println("4)cpp");
+			System.out.println("5)javascript");
+			app = Integer.parseInt(input.nextLine());
+			switch (app) {
+				case 1:
+					py = true;
+					break;
+				case 2:
+					java = true;
+					break;
+				case 3:
+					c = true;
+					break;
+				case 4:
+					cpp = true;
+					break;
+				case 5:
+					j_s = true;
+					break;
+			}
+			System.out.println("To add another programming language pls press 1:");
+			app2 = Integer.parseInt(input.nextLine());
+		} while (app2 == 1);
+		Languages prog_language = new Languages(py, java, c, cpp, j_s);
+		System.out.println("enter more information");
+		String extra_inf = input.nextLine();
+		_employee = new Employee(personal_inf, prog_language, extra_inf);
+	}
 
+}// end of class
